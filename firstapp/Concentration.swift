3d4fe2712c623b4307ade9ca5862c36c325c6ae0
thusitha <thusitha.4t@gmail.com>
2020-06-17 07:24:23 +0530

    game play added

--- firstapp/Concentration.swift
+++ firstapp/Concentration.swift
@@ -12,12 +12,34 @@ class Concentration {
 //    var cards = Array<Card>() //we can add parentheses to init - this will create empty array
     
     var cards = [Card]()
+    var indexOfOneAndOnlyFaceUpCard: Int?
     
     func chooseCards(at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
+//        if cards[index].isFaceUp {
+//            cards[index].isFaceUp = false
+//        } else {
+//            cards[index].isFaceUp = true
+//        }
+        
+        //game play
+        if !cards[index].isMatched { //there is 3 options
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if card match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                //either no card or two card face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
     }
     
