a844d00f51bc2364cbab4368f8a9cb26cbf6af52
thusitha <thusitha.4t@gmail.com>
2020-07-25 22:25:36 +0530

    Different storyboard and navigation

---
+++ 06_Proj_navigation/navigationsystem/navigationsystem/CustomNameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomNameViewController.swift
+//  navigationsystem
+//
+//  Created by thusitha on 7/25/20.
+//  Copyright © 2020 nibm. All rights reserved.
+//
+
+import UIKit
+
+class CustomNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
