917efe1439a604a9abb8bcc49c2f47803e1f9ad9
thusitha <thusitha.4t@gmail.com>
2020-07-25 23:15:28 +0530

    JSON Decode

---
+++ 07_Proj_RestApi/restapi/restapi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  restapi
+//
+//  Created by thusitha on 7/25/20.
+//  Copyright © 2020 nibm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
