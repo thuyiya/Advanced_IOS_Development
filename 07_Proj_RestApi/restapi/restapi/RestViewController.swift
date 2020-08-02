a623bd4351fa7c6c6989987e3768cc077d24957a
thusitha <thusitha.4t@gmail.com>
2020-08-02 09:46:50 +0530

    Update with http client

---
+++ 07_Proj_RestApi/restapi/restapi/RestViewController.swift
@@ -0,0 +1,98 @@
+import UIKit
+
+struct Course: Decodable {
+    var id: Int?
+    var name: String?
+    var link: String?
+    var imageUrl: String?
+}
+
+struct Post: Decodable {
+    var id: Int?
+    var userId: Int?
+    var title: String?
+    var body: String?
+}
+
+struct WebsiteDescription: Decodable {
+    let name: String
+    let description: String
+    let courses: [Course]
+}
+
+
+class RestViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let jsonUrlString = "https://jsonplaceholder.typicode.com/posts"
+        
+        
+        //        guard let url = URL(string: jsonUrlString) else { return }
+        
+        
+        var paramsDictionary = [String:Any]()
+        
+        paramsDictionary["userId"] = 12312
+        paramsDictionary["id"]    = 21412
+        paramsDictionary["title"]    = "titie 1"
+        paramsDictionary["body"]    = "body 1"
+        
+        HttpClientApi.instance().makeAPICall(url: jsonUrlString, params: paramsDictionary, method: .POST, success: { (data, response, error) in
+            print("data loaded")
+            
+            guard let data = data else { return }
+            
+            do {
+                let post = try JSONDecoder().decode(Post.self, from: data)
+                
+                print(post)
+                
+            } catch let jsonError {
+                print("Decode Error ", jsonError)
+            }
+        }) { (data, response, error) in
+            print("wrong api call", error)
+        }
+        
+        
+        
+        
+        
+        //        URLSession.shared.dataTask(with: url) { (data, response, error) in
+        //            // if erro
+        //            // status 200
+        //
+        //            guard let data = data else { return }
+        ////
+        ////            let dataString = String(data: data, encoding: .utf8)
+        //
+        //            do {
+        //                //swift 4 and above
+        //                let courses = try JSONDecoder().decode([Course].self, from: data)
+        //
+        //                print(courses)
+        //
+        //
+        //                //Swift 2/3/ObjectC
+        //
+        ////                guard let josn = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
+        ////
+        ////                let course = Course(json: josn)
+        ////
+        ////                print(course.id)
+        ////                print(course.name)
+        ////                print(course.imageUrl)
+        //
+        //            } catch let jsonError {
+        //                print("Decode Error ", jsonError)
+        //            }
+        //
+        //        }.resume()
+        //
+        //    }
+        
+    }
+    
+}
--- 07_Proj_RestApi/restapi/restapi/ViewController.swift
+++ 07_Proj_RestApi/restapi/restapi/ViewController.swift
@@ -1,79 +1,79 @@
+////
+////  ViewController.swift
+////  restapi
+////
+////  Created by thusitha on 7/25/20.
+////  Copyright © 2020 nibm. All rights reserved.
+////
 //
-//  ViewController.swift
-//  restapi
+//import UIKit
 //
-//  Created by thusitha on 7/25/20.
-//  Copyright © 2020 nibm. All rights reserved.
+//struct WebsiteDescription: Decodable {
+//    let name: String
+//    let description: String
+//    let courses: [Course]
+//}
 //
-
-import UIKit
-
-struct WebsiteDescription: Decodable {
-    let name: String
-    let description: String
-    let courses: [Course]
-}
-
-struct Course: Decodable {
-    let id: Int?
-    let name: String?
-    let link: String?
-    let imageUrl: String?
-    
-    init(json: [String: Any]) {
-        id = json["id"] as? Int ?? -1
-        name = json["name"] as? String ?? ""
-        link = json["link"] as? String ?? ""
-        imageUrl = json["imageUrl"] as? String ?? ""
-        
-    }
-}
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
-        guard let url = URL(string: jsonUrlString) else { return } //optional binding
-        
-        URLSession.shared.dataTask(with: url) { (data, response, err) in
-            //check err
-            //check respose status is 200 (success)
-            
-//            print("do something")
-            
-            guard let data = data else { return }
-            
-            let dataString = String(data: data, encoding: .utf8)
-            
-//            print(dataString)
-            
-            do {
-                //swift 4 and above
-                let courses = try JSONDecoder().decode([Course].self, from: data)
-                print(courses)
-                
-                //Swift 2/3/ObjectC
-//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
-//                let course = Course(json: json)
+//struct Course: Decodable {
+//    let id: Int?
+//    let name: String?
+//    let link: String?
+//    let imageUrl: String?
+//    
+//    init(json: [String: Any]) {
+//        id = json["id"] as? Int ?? -1
+//        name = json["name"] as? String ?? ""
+//        link = json["link"] as? String ?? ""
+//        imageUrl = json["imageUrl"] as? String ?? ""
+//        
+//    }
+//}
 //
-//                print(course.name)
-                
-            } catch let jsonErr {
-                print("Error JSONSerialization", jsonErr)
-            }
-            
-        }.resume()
-        
-//        let myCourse = Course(id: 1, name: "Thusitha", link: "link#", imageUrl: "image url")
-//        print(myCourse)
-    }
-
-
-}
-
-//1 course https://api.letsbuildthatapp.com/jsondecodable/course
-//2 course https://api.letsbuildthatapp.com/jsondecodable/courses
-//3 website_description https://api.letsbuildthatapp.com/jsondecodable/website_description
-//4 course missing https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields
+//class ViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
+//        guard let url = URL(string: jsonUrlString) else { return } //optional binding
+//        
+//        URLSession.shared.dataTask(with: url) { (data, response, err) in
+//            //check err
+//            //check respose status is 200 (success)
+//            
+////            print("do something")
+//            
+//            guard let data = data else { return }
+//            
+//            let dataString = String(data: data, encoding: .utf8)
+//            
+////            print(dataString)
+//            
+//            do {
+//                //swift 4 and above
+//                let courses = try JSONDecoder().decode([Course].self, from: data)
+//                print(courses)
+//                
+//                //Swift 2/3/ObjectC
+////                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
+////                let course = Course(json: json)
+////
+////                print(course.name)
+//                
+//            } catch let jsonErr {
+//                print("Error JSONSerialization", jsonErr)
+//            }
+//            
+//        }.resume()
+//        
+////        let myCourse = Course(id: 1, name: "Thusitha", link: "link#", imageUrl: "image url")
+////        print(myCourse)
+//    }
+//
+//
+//}
+//
+////1 course https://api.letsbuildthatapp.com/jsondecodable/course
+////2 course https://api.letsbuildthatapp.com/jsondecodable/courses
+////3 website_description https://api.letsbuildthatapp.com/jsondecodable/website_description
+////4 course missing https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields
