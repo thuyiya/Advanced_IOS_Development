c46c3e1fcf32cee91e02197431cff8501b8e58c6
thusitha <thusitha.4t@gmail.com>
2020-08-08 14:28:33 +0530

    sign up page

---
+++ 07_Proj_WorkWithFirebase/NibmTaxi/NibmTaxi/View/AuthButtonUIButton.swift
@@ -0,0 +1,21 @@
+//
+//  AuthButtonUIButton.swift
+//  NibmTaxi
+//
+//  Created by thusitha on 8/8/20.
+//  Copyright © 2020 nibm. All rights reserved.
+//
+
+import UIKit
+
+class AuthButtonUIButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
