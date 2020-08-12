b406d41933b2ea88ff2e130d18616f7fd0d30c5c
thusitha <thusitha.4t@gmail.com>
2020-08-12 14:03:47 +0530

    table view cell added

---
+++ 07_Proj_WorkWithFirebase/NibmTaxi/NibmTaxi/View/LocationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationTableViewCell.swift
+//  NibmTaxi
+//
+//  Created by thusitha on 8/12/20.
+//  Copyright © 2020 nibm. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
