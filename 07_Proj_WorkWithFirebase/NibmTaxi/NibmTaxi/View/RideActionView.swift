1be40c4b056f2ebbc0559b054d57a18cab69a0ac
thusitha <thusitha.4t@gmail.com>
2020-08-14 10:12:02 +0530

    ride action view

---
+++ 07_Proj_WorkWithFirebase/NibmTaxi/NibmTaxi/View/RideActionView.swift
@@ -0,0 +1,21 @@
+//
+//  RideActionView.swift
+//  NibmTaxi
+//
+//  Created by thusitha on 8/14/20.
+//  Copyright © 2020 nibm. All rights reserved.
+//
+
+import UIKit
+
+class RideActionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
