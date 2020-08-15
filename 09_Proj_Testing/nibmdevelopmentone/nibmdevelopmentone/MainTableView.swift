ef51ee0d0e2c082f769967c1bf2105059abfbc00
thusitha <thusitha.4t@gmail.com>
2020-08-15 06:22:31 +0530

    basic project

--- 09_Proj_Testing/nibmdevelopmentone/nibmdevelopmentone/MainTableView.swift
+++ 09_Proj_Testing/nibmdevelopmentone/nibmdevelopmentone/MainTableView.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2020 nibm. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MainTableView: UITableViewController {
+    @IBOutlet weak var downloadsCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "NIBM Music"
+        
+        downloadsCell.isHidden = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if UserDefaults.standard.bool(forKey: "loggedIn") {
+            downloadsCell.isHidden = false
+        } else {
+            downloadsCell.isHidden = true
+        }
+    }
+    
+    @IBAction func showLogin(_ sender: Any) {
+        
+        if let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as? ViewController {
+            present(loginVC, animated: true, completion: nil)
+        }
+    }
+
+}
--- 09_Proj_Testing/nibmdevelopmentone/nibmdevelopmentone/ViewController.swift
+++ 09_Proj_Testing/nibmdevelopmentone/nibmdevelopmentone/ViewController.swift
@@ -9,12 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var loadingActivity: UIActivityIndicatorView!
+    @IBOutlet weak var statusLabel: UILabel!
+    let expectedUserName = "CodePro"
+    let expectedPassword = "abc123"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        userName.delegate = self
+        password.delegate = self
+        statusLabel.text = ""
+        loadingActivity.hidesWhenStopped = true
     }
-
-
+    
+    @IBAction func login(_ sender: Any) {
+        
+        statusLabel.text = "Logging In"
+        
+        loadingActivity.startAnimating()
+        
+        guard let userName = userName.text, !userName.isEmpty,
+            let password = password.text, !password.isEmpty else {
+                presentAlert(with: "Missing Credentials", message: "Missing User Name Or Password")
+                return
+        }
+        
+        guard expectedUserName == userName else {
+            presentAlert(with: "Invalid Credentials", message: "Invalid User Name")
+            return
+        }
+        
+        guard expectedPassword == password else {
+            presentAlert(with: "Invalid Credentials", message: "Invalid Password")
+            return
+        }
+        
+        UserDefaults.standard.set(true, forKey: "loggedIn")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.statusLabel.text = "Logged In"
+            self.loadingActivity.stopAnimating()
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    private func presentAlert(with title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alertVC.addAction(.init(title: "Ok", style: .default, handler: nil))
+        
+        present(alertVC, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
