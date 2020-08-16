9241d6ae26b2b8fbdcf53c38220e8532a8e84ee2
thusitha <thusitha.4t@gmail.com>
2020-08-16 10:15:53 +0530

    update

--- 12_Proj_Instruments/RetainCycles/RetainCycles/ViewController.swift
+++ 12_Proj_Instruments/RetainCycles/RetainCycles/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
+class Service {
+     var redController: RedViewController?
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .lightGray
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Show Red", style: .plain, target: self, action: #selector(handleShowRedController))
     }
 
+    @objc func handleShowRedController() {
+        navigationController?.pushViewController(RedViewController(), animated: true)
+    }
 
+}
+
+//lets creat another view controller
+
+class RedViewController: UITableViewController {
+    //de inizilize corectly
+    deinit {
+        print("OS reclaiming memory for Red Controller")
+    }
+    
+    let service = Service()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+        service.redController = self //after you did it it will not work
+    }
 }
 
