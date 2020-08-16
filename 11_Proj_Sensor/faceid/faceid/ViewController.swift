2760e1206fcbdcd85b77a7a2c1166edc34d4d9d9
thusitha <thusitha.4t@gmail.com>
2020-08-16 13:51:57 +0530

    face id added

--- 11_Proj_Sensor/faceid/faceid/ViewController.swift
+++ 11_Proj_Sensor/faceid/faceid/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func athunticate(_ sender: Any) {
+        
+    let context = LAContext()
+    var error: NSError?
 
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+        let reason = "Identify yourself!"
+
+        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+            [weak self] success, authenticationError in
+
+            DispatchQueue.main.async {
+                if success {
+                    let ac = UIAlertController(title: "Authentication success", message: "Well Done", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "Happy", style: .default))
+                    self?.present(ac, animated: true)
+                } else {
+                    let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    self?.present(ac, animated: true)
+                }
+            }
+        }
+    } else {
+        let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(ac, animated: true)
+    }
+    }
+    
 }
 
